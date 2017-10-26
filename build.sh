757fdeb7866a3a9ec4348ea0c92983ce15b8df49
Nyiri Tamas <info@nyiritamas.hu>
2017-10-27 01:11:29 +0200

    travis support

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Builds an example forum with placeholder objects
+# and an admin user with username 'admin', password 'password'.
+#
+# Requires that a postgreSQL database has been created for the forum.
+#
+# If you have used Pony Forum before, make sure that there is no
+# forum/migrations/ folder, since that will break this script.
+set -e # stops execution on error
+rm -rf forum/migrations
+python manage.py schemamigration forum --initial
+python manage.py syncdb --noinput
+python manage.py migrate jatszohaz
+python _postinstall/mkadmin.py
+python _postinstall/definesite.py
+python _postinstall/mkplaceholders.py
+python manage.py test
--- jatszohaz/jatszohaz/settings.py
+++ jatszohaz/jatszohaz/settings.py
@@ -100,6 +100,19 @@ DATABASES = {
     }
 }
 
+# Travis setup
+if get_env_variable('TRAVIS') is not None:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
