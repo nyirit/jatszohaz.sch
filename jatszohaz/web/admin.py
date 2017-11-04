9fd36658f14e05424380f1ba8b175832118ac5f0
Nyiri Tamas <info@nyiritamas.hu>
2017-11-04 15:54:08 +0100

    add models to admin.py

--- jatszohaz/web/admin.py
+++ jatszohaz/web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(GameGroup)
+admin.site.register(GamePiece)
+admin.site.register(GamePack)
+admin.site.register(InventoryItem)
+admin.site.register(Rent)
+admin.site.register(RentActions)
+admin.site.register(Comment)
