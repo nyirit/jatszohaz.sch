bc01657f9dd51b11ec411f75c5435ed588ed0450
Viktor <novk.viktor@gmail.com>
2018-01-25 21:47:53 +0100

    Add news app

--- jatszohaz/jatszohaz/urls.py
+++ jatszohaz/jatszohaz/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     url(r'^rent/', include('rent.urls')),
     url(r'^inventory/', include('inventory.urls')),
 
+    url(r'^news/', include('news.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if 'rosetta' in settings.INSTALLED_APPS:
