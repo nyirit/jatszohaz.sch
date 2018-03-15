1ce6ce2962a4d50fcfcf9d49384869f46a5ba768
Nyíri Tamás <info@nyiritamas.hu>
2018-03-16 00:36:13 +0100

    docker

---
+++ misc/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o errexit
+set -o xtrace
+
+git clone https://github.com/bubi24/jatszohaz.sch.git
+
+virtualenv jh
+source jh/bin/activate
+
+cd jatszohaz.sch
+pip install -r requirements.txt
+
+cd jatszohaz
+python3 manage.py migrate
+python3 manage.py collectstatic
+python3 manage.py compilemessages
+
+gunicorn \
+    jatszohaz.wsgi:application \
+    --bind 0.0.0.0:8000
