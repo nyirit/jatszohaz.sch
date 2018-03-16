7df49a62b5129348c2aff57f68c751a3d191eb60
Nyíri Tamás <info@nyiritamas.hu>
2018-03-16 15:08:21 +0100

    add WhiteNoise

--- jatszohaz/jatszohaz/settings/base.py
+++ jatszohaz/jatszohaz/settings/base.py
@@ -88,6 +88,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
