dfddeb0ff43b33ec8cb5ef70a519272a2a6627d7
Nyíri Tamás <info@nyiritamas.hu>
2018-05-22 18:52:53 +0200

    notification about pending rents

--- jatszohaz/cron/views.py
+++ jatszohaz/cron/views.py
@@ -1,7 +1,11 @@
+from datetime import datetime, timedelta
 import logging
+from urllib.parse import urljoin
 from django.conf import settings
 from django.http import HttpResponse
 from django.views import View
+from jatszohaz.utils import send_slack_message
+from rent.models import Rent
 
 
 logger = logging.getLogger(__name__)
@@ -11,9 +15,19 @@ class Run(View):
     http_method_names = ['get', ]
 
     def get(self, request, *args, **kwargs):
+        # check cron token
         if not settings.CRON_TOKEN or not settings.CRON_TOKEN == kwargs.get('token'):
             logger.error("Invalid cron token!")
             return HttpResponse("Invalid token.")
 
-        logger.info("Cron finished successfully.")
+        # send notification about pending rents
+        from_date = datetime.now() - timedelta(1)
+        rents = Rent.objects.filter(created__lte=from_date).filter(status=Rent.STATUS_PENDING[0])
+        if rents:
+            context = {'rents':  [urljoin(settings.SITE_DOMAIN, str(r.get_absolute_url()))
+                                  for r in rents.all()]}
+            send_slack_message('slack/pending_rents.html', context)
+            logger.info("Pending rents processed.")
+
+        logger.info("Cron finished.")
         return HttpResponse("OK")
