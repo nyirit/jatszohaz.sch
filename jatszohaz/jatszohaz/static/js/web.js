948113fe716a1f7f640827d315dd8cf791d8c2b5
Nyíri Tamás <info@nyiritamas.hu>
2018-06-03 19:25:02 +0200

    add filter ui

--- jatszohaz/jatszohaz/static/js/web.js
+++ jatszohaz/jatszohaz/static/js/web.js
@@ -1,4 +1,3 @@
-//var lang = getCookie("django_language");
 $(".datetimepicker").datepicker({
     dateFormat: 'yy-mm-dd',
     firstDay: 1
@@ -6,4 +5,19 @@ $(".datetimepicker").datepicker({
 
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+    $(".slider").bootstrapSlider();
+
+    slider_player_filter = $("#slider-player-filter");
+    slider_player_filter.on('change' ,function(){
+        val = slider_player_filter.val().split(',');
+        min_value = parseInt(val[0]);
+        max_value = parseInt(val[1]);
+        console.log(min_value + " " + max_value);
+        $(".game_container").filter(function(){
+                return parseInt($(this).attr('data-min-players')) > max_value || parseInt($(this).attr('data-max-players')) < min_value;
+            }).fadeOut(500);
+        $(".game_container").filter(function(){
+                return !(parseInt($(this).attr('data-min-players')) > max_value || parseInt($(this).attr('data-max-players')) < min_value);
+            }).fadeIn(500);
+    });
 });
