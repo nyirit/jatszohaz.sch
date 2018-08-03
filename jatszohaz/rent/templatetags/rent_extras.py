cfb75917614c9681b3276274d0028120ade1533d
Nyíri Tamás <info@nyiritamas.hu>
2018-08-03 14:49:56 +0200

    Confirm before cancel a rent #18

--- jatszohaz/rent/templatetags/rent_extras.py
+++ jatszohaz/rent/templatetags/rent_extras.py
@@ -1,9 +1,12 @@
 from django import template
 
+from rent.models import Rent
+
 register = template.Library()
 
 
 @register.inclusion_tag('rent/rent_statuses.html')
 def show_rent_available_statuses(rent, user, *args):
     return {'statuses': rent.get_available_statuses(user),
-            'rent_pk': rent.pk}
+            'rent_pk': rent.pk,
+            'confirm_statuses': (Rent.STATUS_CANCELLED[0], )}
