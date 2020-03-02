2fe8e4def8c20a16cba623ab62001b2f7f629872
Tamas Nyiri <info@nyiritamas.hu>
2020-03-02 20:34:45 +0100

    add dev user account

--- jatszohaz/jatszohaz/management/commands/create_dev_admin.py
+++ jatszohaz/jatszohaz/management/commands/create_dev_admin.py
@@ -9,5 +9,8 @@ class Command(BaseCommand):
         try:
             JhUser.objects.create_superuser('admin', 'admin@example.com', 'admin')
             self.stdout.write(self.style.SUCCESS("Superuser created: admin/admin"))
+
+            JhUser.objects.create_user('user', 'user@example.com', 'user')
+            self.stdout.write(self.style.SUCCESS("User created: user/user"))
         except IntegrityError:
             self.stdout.write(self.style.SUCCESS("Superuser with username admin already exists."))
