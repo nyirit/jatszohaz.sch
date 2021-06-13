160fac91be0cce21460d0490e8c9544879248fb2
Tamas Nyiri <info@nyiritamas.hu>
2021-06-13 21:08:14 +0200

    turn off RENT_RETURN_DELAY_DAYS

--- jatszohaz/jatszohaz/settings/base.py
+++ jatszohaz/jatszohaz/settings/base.py
@@ -271,7 +271,7 @@ EDU_PERSON_ENTITLEMENT_IGNORE_STATUS = get_env_variable('EDU_PERSON_ENTITLEMENT_
 CRON_TOKEN = get_env_variable('CRON_TOKEN', '')
 
 # number of days while a game cannot be rented again after brought back
-# (due to safety measures regarding COVID-19)
-RENT_RETURN_DELAY_DAYS = 3
+# (initially implemented due to safety measures regarding COVID-19)
+RENT_RETURN_DELAY_DAYS = 0
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
