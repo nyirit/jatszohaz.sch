36e74d31908dc4402c0a4f267194a29a5aedd36c
Tamas Nyiri <info@nyiritamas.hu>
2022-02-22 12:05:26 +0100

    add names for dev users

--- jatszohaz/jatszohaz/management/commands/create_dev_admin.py
+++ jatszohaz/jatszohaz/management/commands/create_dev_admin.py
@@ -7,10 +7,10 @@ from jatszohaz.models import JhUser
 class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
-            JhUser.objects.create_superuser('admin', 'admin@example.com', 'admin')
+            JhUser.objects.create_superuser('admin', 'admin@example.com', 'admin', first_name='Admin', last_name='Admin')
             self.stdout.write(self.style.SUCCESS("Superuser created: admin/admin"))
 
-            JhUser.objects.create_user('user', 'user@example.com', 'user')
+            JhUser.objects.create_user('user', 'user@example.com', 'user', first_name='User', last_name='User')
             self.stdout.write(self.style.SUCCESS("User created: user/user"))
         except IntegrityError:
             self.stdout.write(self.style.SUCCESS("Superuser with username admin already exists."))
